12cd00b10c1c3b9f2436d3fa76c12a06a9f9aa68
Ульяна <skipina_ulyana24@mail.ru>
2023-11-07 00:04:44 +0300

    amendment

--- src/main/java/ru/volkova/multilenghtapp/_main/_Main.java
+++ src/main/java/ru/volkova/multilenghtapp/_main/_Main.java
@@ -3,21 +3,11 @@ package ru.volkova.multilenghtapp._main;
 import ru.volkova.multilenghtapp.service.MultiplicationTableService;
 import ru.volkova.multilenghtapp.service.SegmentLengthService;
 
-import java.util.Scanner;
-
 public class _Main {
     public static void main(String[] args) {
         MultiplicationTableService multiplicationTable = new MultiplicationTableService();
-        multiplicationTable.printMultiplyTable();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите параметры первой точки: x1 y1");
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        System.out.println("Введите параметры второй точки: x2 y2");
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
         SegmentLengthService segmentLength = new SegmentLengthService();
-        double result = segmentLength.calculateSegmentLen(x1, y1, x2, y2);
-        System.out.println("Длина отрезка = " + result);
+        multiplicationTable.printMultiplyTable();
+        segmentLength.printSegmentLen();
     }
 }
--- src/main/java/ru/volkova/multilenghtapp/service/SegmentLengthService.java
+++ src/main/java/ru/volkova/multilenghtapp/service/SegmentLengthService.java
@@ -1,9 +1,23 @@
 package ru.volkova.multilenghtapp.service;
 
+import java.util.Scanner;
+
 public class SegmentLengthService {
-    public double calculateSegmentLen(int x1, int y1, int x2, int y2) {
+    public static double calculateSegmentLen(int x1, int y1, int x2, int y2) {
         double distance = Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
         return distance;
+    }
+
+    public void printSegmentLen() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите параметры первой точки: x1 y1");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        System.out.println("Введите параметры второй точки: x2 y2");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        double result = SegmentLengthService.calculateSegmentLen(x1, y1, x2, y2);
+        System.out.println("Длина отрезка = " + result);
     }
 }
 
